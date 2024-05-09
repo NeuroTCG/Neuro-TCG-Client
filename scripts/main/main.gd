e2ff45237fe894cefd66ceb009e0f1740c78d3e5
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2024-05-09 17:59:44 +0200

    Add a send_command function.
    Add launch.json.

--- scripts/main/main.gd
+++ scripts/main/main.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+var version = "early development build"
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	User.client = Client.new()
 	add_child(User.client)
 	await User.client.wait_until_connection_opened()
-	User.client.ws.send_text("version,early development build")
+	User.send_command("version", [version])
--- scripts/networking/user.gd
+++ scripts/networking/user.gd
@@ -1,3 +1,29 @@
 extends Node
 
 var client: Client
+
+class Command:
+	func _init(name_: String, n_args: int):
+		name = name_
+		num_args = n_args
+		
+	var name: String
+	var num_args: int
+
+var commands: Array[Command] = [
+	Command.new("version", 1),
+	Command.new("summon", 3),
+	Command.new("attack", 4),
+]
+
+func send_command(command: String, arguments: Array[String]):
+	var command_exists = false
+	for cmd in commands:
+		if command == cmd.name:
+			command_exists = true
+			assert(len(arguments) == cmd.num_args, "Got %d arguments for command '%s'. Expected %d" % [len(arguments), command, cmd.num_args])
+			break
+
+	assert(command_exists, "Command '%s' does not exist" % command)
+
+	client.ws.send_text(",".join([command] + arguments))
