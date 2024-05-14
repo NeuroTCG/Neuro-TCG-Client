257ec1e576a0f6bac0155b0abae60d036cca891d
Superbox2147 <132390694+Superbox2147@users.noreply.github.com>
2024-05-14 14:39:39 +0300

    Add a basic audio test that plays a song when starting
    TODO: make the song that plays configurable in the settings.
    Make sure the song loops correctly

--- scripts/main/main.gd
+++ scripts/main/main.gd
@@ -4,10 +4,12 @@ var version = "early development build"
 
 var main_menu_template = preload("res://scenes/ui/main_menu.tscn")
 var loading_screen_template = preload("res://scenes/ui/loading_screen.tscn")
+@export var music_file : AudioStream
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_child(main_menu_template.instantiate())
+	AudioSystem.play_music(music_file)
 	
 	User.client = Client.new()
 	add_child(User.client)
