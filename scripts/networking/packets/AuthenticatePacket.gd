b83971535facadbb2f44a8d1eb36639467cda526
Superbox2147 <132390694+Superbox2147@users.noreply.github.com>
2024-10-25 16:30:41 +0300

    add incomplete OAuth implementation
    Just changes to the classes to make sure the client should still work

--- scripts/networking/packets/AuthenticatePacket.gd
+++ scripts/networking/packets/AuthenticatePacket.gd
@@ -2,19 +2,25 @@ extends Packet
 class_name AuthenticatePacket
 
 var username: String
+var has_oauth: bool
+var oauth_data: String
 
 
-func _init(username_: String) -> void:
+func _init(username_: String, has_oauth_: bool = false, oauth_data_: String = "") -> void:
 	super(PacketType.Authenticate)
 	username = username_
+	has_oauth = has_oauth_
+	oauth_data = oauth_data_
 
 
 func to_dict() -> Dictionary:
 	return {
 		"type": type,
 		"username": username,
+		"hasOAuth": has_oauth,
+		"oAuthData": oauth_data,
 	}
 
 
 static func from_dict(d: Dictionary) -> AuthenticatePacket:
-	return AuthenticatePacket.new(d["username"])
+	return AuthenticatePacket.new(d["username"], d["hasOAuth"], d["oAuthData"])
--- scripts/networking/packets/AuthenticationValidPacket.gd
+++ scripts/networking/packets/AuthenticationValidPacket.gd
@@ -3,12 +3,18 @@ class_name AuthenticationValidPacket
 
 var has_running_game: bool
 var you: UserInfo
+var has_new_tokens: bool
+var new_access_token: String
+var new_refresh_token: String
 
 
-func _init(has_running_game_: bool, you_: UserInfo) -> void:
+func _init(has_running_game_: bool, you_: UserInfo, has_new_tokens_: bool = false, new_access_token_: String = "", new_refresh_token_: String = "") -> void:
 	super(PacketType.AuthenticationValid)
 	has_running_game = has_running_game_
 	you = you_
+	has_new_tokens = has_new_tokens_
+	new_access_token = new_access_token_
+	new_refresh_token = new_refresh_token_
 
 
 func to_dict() -> Dictionary:
@@ -16,8 +22,11 @@ func to_dict() -> Dictionary:
 		"type": type,
 		"has_running_game": has_running_game,
 		"you": you.to_dict(),
+		"hasNewTokens": has_new_tokens,
+		"newAccessToken": new_access_token,
+		"newRefreshToken": new_refresh_token,
 	}
 
 
 static func from_dict(d: Dictionary) -> AuthenticationValidPacket:
-	return AuthenticationValidPacket.new(d["has_running_game"], UserInfo.from_dict(d["you"]))
+	return AuthenticationValidPacket.new(d["has_running_game"], UserInfo.from_dict(d["you"]), d["hasNewTokens"], d["newAccessToken"], d["newRefreshToken"])
