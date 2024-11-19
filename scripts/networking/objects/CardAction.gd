175d0e9805eec605afe6f35772c1aebcf3b7a9b4
Blast Studio Games <mattclifton10@gmail.com>
2024-11-18 19:57:52 -0500

    added CardActionTarget object

--- scripts/networking/objects/CardAction.gd
+++ scripts/networking/objects/CardAction.gd
@@ -1,18 +1,18 @@
 class_name CardAction
 
 var action_name: String
-var targets: Array[CardPosition]
+var targets: Array[CardActionTarget]
 var amount: int
 
 
-func _init(_action_name: String, _targets: Array[CardPosition], _amount: int):
+func _init(_action_name: String, _targets: Array[CardActionTarget], _amount: int):
 	action_name = _action_name
 	targets = _targets
 	amount = _amount
 
 
 static func from_dict(d: Dictionary) -> CardAction:
-	var target_array: Array[CardPosition] = []
+	var target_array: Array[CardActionTarget] = []
 	for t in d["targets"]:
-		target_array.append(CardPosition.from_array(t))
+		target_array.append(CardActionTarget.from_dict(t))
 	return CardAction.new(d["action_name"], target_array, d["amount"])
