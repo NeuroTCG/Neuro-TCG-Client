dd5a80be3a9b337e047595e04e7450e3dd3fc293
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2025-02-16 23:23:27 +0100

    Handle disconnects on the loading screen

--- scripts/ui/loading_screen.gd
+++ scripts/ui/loading_screen.gd
@@ -2,6 +2,7 @@ extends Control
 class_name LoadingScreen
 
 var game_node := load("res://scenes/game/game.tscn")
+var game_over_template = preload("res://scenes/ui/game_over.tscn")
 
 var loading_text: String = "Loading...":
 	set(new_text):
@@ -16,7 +17,7 @@ func _ready() -> void:
 	Global.add_child(Global.network_manager)
 	Global.network_manager_ready.emit()
 	Global.network_manager.match_found.connect(__on_match_found)
-	Global.network_manager.disconnect.connect(__tmp_on_disconnect)
+	Global.network_manager.disconnect.connect(_on_disconnect)
 	Global.network_manager.client_info_accept.connect(__on_client_info_accept)
 	Global.network_manager.authentication_valid.connect(__on_authentication_valid)
 	Global.network_manager.connection.connection_failed.connect(__on_connection_failed)
@@ -49,5 +50,16 @@ func __on_connection_failed(url: String, error: String) -> void:
 	loading_text = "Connecting to %s failed: %s" % [url, error]
 
 
-func __tmp_on_disconnect(packet: DisconnectPacket) -> void:
-	print("Disconnected with message %s" % packet.message)
+func _on_disconnect(packet: DisconnectPacket) -> void:
+	load_game_over("Disconnect packet received: %s (%s)" % [packet.message, packet.reason])
+
+
+func load_game_over(message: String) -> void:
+	var game_over = game_over_template.instantiate()
+
+	game_over.get_node("VBoxContainer/WinLabel").text = message
+	get_tree().root.add_child(game_over)
+
+	queue_free()
+	# stop any connections
+	Global.network_manager.queue_free()
