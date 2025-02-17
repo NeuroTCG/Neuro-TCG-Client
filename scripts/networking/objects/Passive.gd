547a464d0d5b5dd20ff0b661878012cc2edd4281
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2025-02-17 13:23:34 +0100

    Make value size depend on the effect.

--- scripts/networking/objects/Passive.gd
+++ scripts/networking/objects/Passive.gd
@@ -8,21 +8,19 @@ enum PassiveEffectType {
 
 var effect: PassiveEffectType
 var values: Array
-var values_size: int
 
-func _init(_effect: PassiveEffectType, _values: Array, _values_size: int) -> void:
+
+func _init(_effect: PassiveEffectType, _values: Array) -> void:
 	effect = _effect
 	values = _values
-	values_size = _values_size
+
 
 func to_dict() -> Dictionary:
 	return {
 		"effect": effect,
 		"values": values,
-		"values_size": values_size,
 	}
 
+
 static func from_dict(d: Dictionary) -> Passive:
-	return Passive.new(
-		PassiveEffectType.get(d["effect"]), d["values"], d["valuesSize"]
-	)
+	return Passive.new(PassiveEffectType.get(d["effect"]), d["values"])
