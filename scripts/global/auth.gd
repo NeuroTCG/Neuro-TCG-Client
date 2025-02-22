cd9f48f55784ee77519fdc1158d7ab41a267c691
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2025-02-22 13:19:21 +0100

    Refactor auth and add development user support.

--- scripts/global/auth.gd
+++ scripts/global/auth.gd
@@ -1,20 +1,88 @@
 extends Node
 
-var token := ""
 var user_info: UserInfo = null
+var token := ""
+var _user_token := ""
+var _development_token := ""
 
 const TOKEN_FILE := "user://token.txt"
 
+const DEFAULT_REQUEST_TIMEOUT = 5
+
 var request = HTTPRequest.new()
+
+var dev_username := ""
 
 
 func _ready() -> void:
+	request.timeout = DEFAULT_REQUEST_TIMEOUT
 	add_child(request)
+
+	var args = OS.get_cmdline_args()
+	for i in range(len(args)):
+		if args[i] == "--dev-username":
+			dev_username = args[i + 1]
+			print("INFO: development username was set by commandline: %s" % dev_username)
+			break
+
+
+## returns (user_login_url, poll_url) or null on error
+func get_login_and_poll_url():
+	await Config.wait_for_server_reachable()
+	var error = request.request(Config.http_server_base + "/auth/begin", [], HTTPClient.METHOD_POST)
+	if error != OK:
+		print("ERROR: failed to start auth session")
+		print(error)
+		return null
+
+	var result = await request.request_completed
+	var response_code: int = result[1]
+	var body: PackedByteArray = result[3]
+
+	if response_code != 200:
+		print("ERROR: /auth/begin returned code %d" % response_code)
+		return null
+
+	var json = JSON.parse_string(body.get_string_from_utf8())
+
+	var user_login_url: String = json["user_login_url"]
+	var poll_url: String = json["poll_url"]
+	print("User login URL: ", user_login_url, " Poll URL: ", poll_url)
+	return [user_login_url, poll_url]
+
+
+func poll_for_token(poll_url: String, headers: Array = []):
+	request.timeout = 20
+	var new_token = null
+	print("Auth polling on %s" % poll_url)
+	while true:
+		request.request(poll_url, headers, HTTPClient.METHOD_GET)
+		var response = await request.request_completed
+		var r_code: int = response[1]
+		var r_body = response[3].get_string_from_utf8()
+		if r_code == 200:
+			print("INFO: Auth polling finished")
+			new_token = r_body
+			break
+		elif r_code == 408 || r_code == 504:
+			print("INFO: Timeout while attempting to authenticate")
+			continue
+		else:
+			print("ERROR: Auth polling returned code %d" % r_code)
+			new_token = null
+			break
+
+	request.timeout = DEFAULT_REQUEST_TIMEOUT
+	return new_token
 
 
 func _refresh_user_info() -> bool:
 	await Config.wait_for_server_reachable()
-	request.request(Config.http_server_base + "/users/@me", ["Authorization: Bearer " + token], HTTPClient.METHOD_GET)
+	request.request(
+		Config.http_server_base + "/users/@me",
+		["Authorization: Bearer " + token],
+		HTTPClient.METHOD_GET
+	)
 	var result = await request.request_completed
 	if result[1] != 200:
 		print("ERROR: failed to get user info for token")
@@ -23,27 +91,90 @@ func _refresh_user_info() -> bool:
 	var body = JSON.parse_string(result[3].get_string_from_utf8())
 
 	user_info = UserInfo.from_dict(body)
-	print("Refreshed user info: %s" % [JSON.stringify(user_info.to_dict())])
+	print("INFO: Refreshed user info: %s" % [JSON.stringify(user_info.to_dict())])
 	return true
 
 
-func set_token_and_save(new_token: String):
-	token = new_token
+func set_user_token_and_save(new_token: String):
+	_user_token = new_token
+	token = _user_token
 	if await _refresh_user_info():
 		var file = FileAccess.open(TOKEN_FILE, FileAccess.WRITE)
-		file.store_string(token)
+		file.store_string(_user_token)
 		file.close()
+
+		if dev_username != "":
+			print("INFO: Upgrading to development user")
+
+			if not await _upgrade_to_development_user():
+				assert(false, "Upgrading to a development account failed")
+
 	else:
 		delete_user_info()
+
+
+func _upgrade_to_development_user() -> bool:
+	if dev_username == "":
+		return false
+
+	var urls = await get_login_and_poll_url()
+	if urls == null:
+		print("ERROR: dev upgrade failed to start auth session")
+		return false
+
+	var correlation_id = urls[0].split("=")[1]
+	var headers = ["Authorization: Bearer " + _user_token]
+	request.request(urls[0], headers)  # simulate the user opening the page
+	var r_result = await request.request_completed
+	if r_result[1] != 200:
+		print("ERROR: dev upgrade login url returned code %d" % r_result[1])
+		return false
+
+	# TODO: don't hardcode this
+	var provider_redirect_url = (
+		Config.http_server_base
+		+ (
+			"/auth/providers/__development/begin?correlationId=%s&devUserId=%s"
+			% [correlation_id, dev_username]
+		)
+	)
+	request.request(provider_redirect_url, headers)
+	r_result = await request.request_completed
+	if r_result[1] != 200:
+		print("ERROR: dev upgrade provider redirect returned code %d" % r_result[1])
+		return false
+
+	var new_token = await poll_for_token(urls[1], headers)
+	if new_token == null:
+		print("ERROR: dev upgrade failed to get token")
+		return false
+
+	_set_development_token(new_token)
+	return true
+
+
+func _set_development_token(new_token: String):
+	_development_token = new_token
+	token = _development_token
+	if not await _refresh_user_info():
+		_development_token = ""
+		token = _user_token
 
 
 func load_token() -> void:
 	var file = FileAccess.open(TOKEN_FILE, FileAccess.READ)
 	if file:
-		token = file.get_as_text()
+		_user_token = file.get_as_text()
+		token = _user_token
 		file.close()
 
-	if !await _refresh_user_info():
+	if await _refresh_user_info():
+		if dev_username != "":
+			print("INFO: Upgrading to development user")
+
+			if not await _upgrade_to_development_user():
+				assert(false, "Upgrading to a development account failed")
+	else:
 		delete_user_info()
 
 
@@ -51,5 +182,7 @@ func delete_user_info():
 	if FileAccess.file_exists(TOKEN_FILE):
 		DirAccess.remove_absolute(TOKEN_FILE)
 
+	_user_token = ""
+	_development_token = ""
 	token = ""
 	user_info = null
--- scripts/login.gd
+++ scripts/login.gd
@@ -8,69 +8,29 @@ var main_menu_template := preload("res://scenes/ui/main_menu.tscn")
 @onready var StatusLabel := $Control/VBoxContainer/StatusLabel
 @onready var AuthMethodsContainer := $Control/VBoxContainer/AuthMethodsContainer
 @onready var URLText := $Control/VBoxContainer/URL
-@onready var request := $HTTPRequest
 @onready var AuthenticatingGroup = $Control/VBoxContainer/Authenticating
-@onready var AfterCopyTimer = $AfterCopyTimer
 
-
-func _save_token_and_load_main_menu(token: String):
-	Auth.set_token_and_save(token)
-	get_parent().add_child(main_menu_template.instantiate())
-	queue_free()
-
-
-func _auth_info_request_completed(_result, response_code, _headers, body):
-	# set timeout and disconnect, we only want to connect once
-	request.set_timeout(20)
-	request.request_completed.disconnect(_auth_info_request_completed)
-
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	if response_code != 200:
-		print("Error contacting server")
-		_toggle_authentication_methods(true)
-		return null
-
-	var user_login_url: String = json["user_login_url"]
-	var poll_url: String = json["poll_url"]
-	print("User login URL: ", user_login_url, " Poll URL: ", poll_url)
-	URLText.text = user_login_url
-
-	while true:
-		request.request(poll_url, [], HTTPClient.METHOD_GET)
-		var response = await request.request_completed
-		var r_code: int = response[1]
-		var r_body = response[3].get_string_from_utf8()
-		if r_code == 200:
-			print("Authenticated with token: ", r_body)
-			_save_token_and_load_main_menu(r_body)
-			break
-		elif r_code == 408 || r_code == 504:
-			print("Timeout while attempting to authenticate")
-			continue
-		else:
-			_toggle_authentication_methods(true)
-			return null
-
-
-func _contact_server():
-	await Config.wait_for_server_reachable()
-	request.request_completed.connect(_auth_info_request_completed)
-	var error = request.request(Config.http_server_base + "/auth/begin", [], HTTPClient.METHOD_POST)
-	if error != OK:
-		print("Error contacting server")
-		return null
+var request := HTTPRequest.new()
 
 
 func _ready():
 	Config.server_status_changed.connect(_on_server_status_changed)
 	Config.refresh_server_status()
-	Auth.load_token()
+	await Auth.load_token()
 	if Auth.token != "":
-		_save_token_and_load_main_menu(Auth.token)
+		_load_main_menu()
 		return
 
+	request.set_timeout(20)
+
+	add_child(request)
+
 	_toggle_authentication_methods(true)
-	AfterCopyTimer.timeout.connect(_reset_status_label_after_copy)
+
+
+func _load_main_menu():
+	get_parent().add_child(main_menu_template.instantiate())
+	queue_free()
 
 
 func _on_server_status_changed(status: Config.ServerStatus) -> void:
@@ -95,14 +55,27 @@ func _toggle_authentication_methods(enabled: bool):
 	AuthenticatingGroup.visible = !enabled
 	if enabled:
 		StatusLabel.text = DEFAULT_LABEL
-		AfterCopyTimer.stop()
 	else:
 		StatusLabel.text = AUTHENTICATING_LABEL
 
 
 func _on_login_button_pressed() -> void:
-	await _contact_server()
 	_toggle_authentication_methods(false)
+
+	var urls = await Auth.get_login_and_poll_url()
+
+	if urls == null:
+		print("Error contacting server")
+		_toggle_authentication_methods(true)
+
+	URLText.text = urls[0]
+	var token = await Auth.poll_for_token(urls[1])
+	if token == null:
+		_toggle_authentication_methods(true)
+	else:
+		print("Authenticated with token: ", token)
+		await Auth.set_user_token_and_save(token)
+		_load_main_menu()
 
 
 func _on_back_pressed() -> void:
@@ -113,14 +86,12 @@ func _on_copy_pressed() -> void:
 	DisplayServer.clipboard_set(URLText.text)
 	StatusLabel.text = "Copied to clipboard!"
 
-	AfterCopyTimer.wait_time = 2.0
-	AfterCopyTimer.one_shot = true
-	AfterCopyTimer.start()
-
-
-func _on_open_pressed() -> void:
-	OS.shell_open(URLText.text)
+	get_tree().create_timer(2).timeout.connect(_reset_status_label_after_copy)
 
 
 func _reset_status_label_after_copy() -> void:
 	StatusLabel.text = AUTHENTICATING_LABEL
+
+
+func _on_open_pressed() -> void:
+	OS.shell_open(URLText.text)
