b51536d42bacbedc1352876c53b58187c27e7073
Robotino04 <100292446+Robotino04@users.noreply.github.com>
2025-07-22 10:41:59 +0000

    Deploying to gh-pages from @ NeuroTCG/Neuro-TCG-Client@acc8fb9548918fccff12d10e3ba8d388400ca512 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749231809|8344441';
+const CACHE_VERSION = '1753180898|8679224';
 /** @type {string} */
 const CACHE_PREFIX = 'Neuro-TCG-client-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
